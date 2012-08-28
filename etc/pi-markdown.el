ac12b26cc0b100dc90a50479563022f2f35b940c
pivaldi <contact@piprime.fr>
2012-08-28 22:06:39 +0200

    Add snippet

--- etc/pi-markdown.el
+++ etc/pi-markdown.el
@@ -3,7 +3,7 @@
 
 (autoload 'markdown-mode "markdown-mode.el"
   "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\(\\.text$\\|\\.mdk$\\|\\.md$\\)" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\(\\.text$\\|\\.mkd$\\|\\.md$\\)" . markdown-mode))
 
 (provide 'pi-markdonw)
 ;;; pi-markdonw.el ends here

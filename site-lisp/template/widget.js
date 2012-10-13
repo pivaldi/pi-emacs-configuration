e7ae16772c467c24aa80965eb1f48c8cdc05c08b
Okapi <okapi@geonef.fr>
2012-10-13 15:02:56 +0200

    JS snippets & templates

---
+++ site-lisp/template/widget.js
@@ -0,0 +1,24 @@
+define([
+  "dojo/_base/declare",
+  "geonef/jig/_Widget",
+  "dojo/_base/lang",
+  "geonef/button/Action"
+], function(declare, _Widget, lang, Action) {
+
+return declare([_Widget], { //--indent after--
+
+  "class": _Widget.prototype["class"] + " myWidget",
+
+  makeContentNodes: function() {
+    return [
+      ["div", {}, "My content"]
+    ];
+  },
+
+  startup: function() {
+    this.inherited(arguments);
+  }
+
+});
+
+});

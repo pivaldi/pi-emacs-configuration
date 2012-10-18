f6979983fdd5efe6e6e007b2f1a9041ea9ead36d
okapi <jf@geonef.fr>
2012-10-18 14:43:17 +0200

    alias _ib, fixed JS templates, added znoi JS snippet

--- site-lisp/template/define.js
+++ site-lisp/template/define.js
@@ -1,10 +1,14 @@
+/**
+ * Description courte du module (obligatoire)
+ *
+ * Description plus longue du module (conseillé)
+ */
 define([
   "dojo/_base/declare",
   "dojo/_base/lang"
 ], function(declare, lang) {
 
-  return declare([_Widget],
-{ //--noindent--
+return declare([_Widget], { //--noindent--
 
 });
 

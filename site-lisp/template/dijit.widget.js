e0af99a98274f9523d190a648f8a70c41ac8b557
pivaldi <contact@piprime.fr>
2014-04-17 10:21:55 +0200

    Fix dijit template (bis)

--- site-lisp/template/dijit.widget.js
+++ site-lisp/template/dijit.widget.js
@@ -1,9 +1,11 @@
 define([
   "module",
   "dojo/_base/declare",
+
   "geonef/jig/_Widget"
 ], function(
-  module, declare, _Widget
+  module, declare,
+  _Widget
 ) {
   return declare([_Widget], {
 

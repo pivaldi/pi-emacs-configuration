0b99456aaadf49e669b40e34575dbd90598d0272
pivaldi <contact@piprime.fr>
2015-09-08 12:17:39 +0200

    Remove web-beautify-html-buffer from before-save-hook

--- etc/pi-web-beautify.el
+++ etc/pi-web-beautify.el
@@ -40,10 +40,10 @@
                (lambda ()
                  (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
 
-  (eval-after-load 'sgml-mode
-    '(add-hook 'html-mode-hook
-               (lambda ()
-                 (add-hook 'before-save-hook 'web-beautify-html-buffer t t))))
+  ;; (eval-after-load 'sgml-mode
+  ;;   '(add-hook 'html-mode-hook
+  ;;              (lambda ()
+  ;;                (add-hook 'before-save-hook 'web-beautify-html-buffer t t))))
 
   (eval-after-load 'css-mode
     '(add-hook 'css-mode-hook

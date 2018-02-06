d734cae19fe545586ed86056ec9b4d3d44cb73c0
pivaldi <contact@piprime.fr>
2018-02-06 17:20:50 +0100

    Elfeed sorting by tags

--- etc/pi-elfeed.el
+++ etc/pi-elfeed.el
@@ -30,6 +30,18 @@
   (when (require 'elfeed-goodies nil t)
     (elfeed-goodies/setup)
     )
+
+  (defun pi-elfeed-tag-sort (a b)
+    "Custom elfeed sort function based on serialization tag list into
+a string, falling back on the date to break ties (descending).
+See https://github.com/skeeto/elfeed/issues/203#issuecomment-335038206"
+    (let* ((a-tags (format "%s" (elfeed-entry-tags a)))
+           (b-tags (format "%s" (elfeed-entry-tags b))))
+      (if (string= a-tags b-tags)
+          (< (elfeed-entry-date b) (elfeed-entry-date a)))
+      (string< a-tags b-tags)))
+
+  (setf elfeed-search-sort-function #'pi-elfeed-tag-sort)
   )
 
 (provide 'pi-elfeed)
